2482149f8f50d21136e074ba17fb2335408474f5
Arie Umarella <al.razli.umarella.15111020@gmail.com>
2022-08-29 23:43:32 +0700

    db

---
+++ baru.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3561fc796075d2843375b1191c3136d680f6b4f168d6c929e5d7f1f532a32af2
+size 407232
--- db.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0c81646a735bf6bcae649f2003c29fa959dc5b8637cae2491733ba5576bf5d0a
-size 335827
