0afd0aaecd6b26a0176ab900b8ca32dc948e218d
Arie Umarella <al.razli.umarella.15111020@gmail.com>
2022-09-08 15:15:16 +0700

    fix scjuling

--- application/controllers/C_schedule.js
+++ application/controllers/C_schedule.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca5ede3caf7dde195713c1b80adf45a1c8dfd3814c847e020407beed8a077931
-size 12373
+oid sha256:2b0a272fc01c72bb92c43d883c3d67472c207542f01c1a0f3799f484af8ebcf7
+size 12665
